7fef7bf8742d7d1d3e2e4c438efcc0daf91b50d8
Andy Kim <youngkim14@gmail.com>
2023-09-11 13:29:34 -0400

    fixed bug in component

--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -1,43 +1,44 @@
-import React, { useRef } from 'react';
-import { useSelector } from 'react-redux';
+import React, { useRef } from "react";
+import { useSelector } from "react-redux";
 
 const MovieCard = ({ onMovieClick }) => {
-
   const { movies } = useSelector((state) => state);
   // console.log('movies', movies);
-  const IMAGE_PATH = 'https://image.tmdb.org/t/p/w500';
+  const IMAGE_PATH = "https://image.tmdb.org/t/p/w500";
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
-      behavior: 'smooth',
+      behavior: "smooth",
     });
   };
-  
-  
-  const handleMovieClick = (movie) => {
-    setSelectedMovie(movie);
-  };
+
+  // const handleMovieClick = (movie) => {
+  //   setSelectedMovie(movie);
+  // };
 
   return (
-    <div className='container'>
+    <div className="container">
       {movies.map((movie) => {
         return (
-          <div 
-            className='movie-card' 
+          <div
+            className="movie-card"
             key={movie.id}
             onClick={() => {
               onMovieClick(movie);
               scrollToTop();
             }}
           >
-            {movie.poster_path ? <img className='movie-cover' src={`${IMAGE_PATH}${movie.poster_path}`} alt=''/>
-              : 
-              <div className='posterless'>
-                Movie Poster Pending
-              </div>
-            }
-          
-            <h5 className ='container-movie-title'>{movie.title}</h5>
+            {movie.poster_path ? (
+              <img
+                className="movie-cover"
+                src={`${IMAGE_PATH}${movie.poster_path}`}
+                alt=""
+              />
+            ) : (
+              <div className="posterless">Movie Poster Pending</div>
+            )}
+
+            <h5 className="container-movie-title">{movie.title}</h5>
           </div>
         );
       })}

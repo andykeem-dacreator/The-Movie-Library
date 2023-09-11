b00e569b2c82e6c51d150359b954c6b01c75db98
Andy Kim <youngkim14@gmail.com>
2023-09-11 12:20:03 -0400

    edited webppack

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,24 @@
-const Dotenv = require('dotenv-webpack');
+const Dotenv = require("dotenv-webpack");
+const path = require("path"); // Add this import
 
 module.exports = {
-  devtool: 'source-map',
-  plugins: [
-    new Dotenv(),
-  ],
+  mode: "development",
+  devtool: "source-map",
+  plugins: [new Dotenv()],
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         options: {
-          presets: ['@babel/preset-react']
-        }
-      }
-    ]
-  }
+          presets: ["@babel/preset-react"],
+        },
+      },
+    ],
+  },
 };
